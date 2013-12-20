430ec44e370734f58142a33df3860ce0795c7f48
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-20 21:06:35 +0100

    Add F# project scaffolding - references #4

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.0.1-alpha - 17.12.2013 
+* Initial release of FSharp.Interop.PythonProvider
---
+++ src/PythonTypeProviderProxy/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("PythonTypeProviderProxy")>]
+[<assembly: AssemblyProductAttribute("PythonTypeProviderProxy")>]
+[<assembly: AssemblyDescriptionAttribute("Early experimental F# type provider for python.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
